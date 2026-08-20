a87da4c24dccaf7c77fd16f31ccdd3f6b02d1232
haseebali-dev <haseeb102178@gmail.com>
2026-08-20 23:27:37 +0500

    pushed

--- backend/CryptoTrading.API/obj/Debug/net8.0/CryptoTrading.API.AssemblyInfo.cs
+++ backend/CryptoTrading.API/obj/Debug/net8.0/CryptoTrading.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoTrading.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1130cf1437a7e6143325b07a3ab449c0c9bfabb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec36098780aaf10b9af5479d53098c271a102b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoTrading.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoTrading.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
